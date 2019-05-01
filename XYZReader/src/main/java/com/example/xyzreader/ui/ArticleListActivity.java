09ef6d2acf64670e1057e878f1a22f776ec898fe
Marouen Chaouch <chaouch.marouen@gmail.com>
2019-05-01 23:25:39 +0100

    Initial commit
    Signed-off-by: Marouen Chaouch <chaouch.marouen@gmail.com>

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
@@ -8,16 +8,16 @@ import android.content.IntentFilter;
 import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.util.DiffUtil;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.text.format.DateUtils;
 import android.util.Log;
-import android.util.TypedValue;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -38,7 +38,7 @@ import java.util.GregorianCalendar;
  * touched, lead to a {@link ArticleDetailActivity} representing item details. On tablets, the
  * activity presents a grid of items as cards.
  */
-public class ArticleListActivity extends ActionBarActivity implements
+public class ArticleListActivity extends AppCompatActivity implements
         LoaderManager.LoaderCallbacks<Cursor> {
 
     private static final String TAG = ArticleListActivity.class.toString();
@@ -51,20 +51,31 @@ public class ArticleListActivity extends ActionBarActivity implements
     private SimpleDateFormat outputFormat = new SimpleDateFormat();
     // Most time functions can only handle 1902 - 2037
     private GregorianCalendar START_OF_EPOCH = new GregorianCalendar(2,1,1);
+    private Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article_list);
 
-        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
 
 
         final View toolbarContainerView = findViewById(R.id.toolbar_container);
 
-        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
+        mSwipeRefreshLayout = findViewById(R.id.swipe_refresh_layout);
 
-        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mRecyclerView = findViewById(R.id.recycler_view);
+
+        adapter = new Adapter(null);
+        adapter.setHasStableIds(true);
+        mRecyclerView.setAdapter(adapter);
+        int columnCount = getResources().getInteger(R.integer.list_column_count);
+        StaggeredGridLayoutManager sglm =
+                new StaggeredGridLayoutManager(columnCount, StaggeredGridLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(sglm);
+
         getLoaderManager().initLoader(0, null, this);
 
         if (savedInstanceState == null) {
@@ -112,13 +123,7 @@ public class ArticleListActivity extends ActionBarActivity implements
 
     @Override
     public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
-        Adapter adapter = new Adapter(cursor);
-        adapter.setHasStableIds(true);
-        mRecyclerView.setAdapter(adapter);
-        int columnCount = getResources().getInteger(R.integer.list_column_count);
-        StaggeredGridLayoutManager sglm =
-                new StaggeredGridLayoutManager(columnCount, StaggeredGridLayoutManager.VERTICAL);
-        mRecyclerView.setLayoutManager(sglm);
+        adapter.updateList(cursor);
     }
 
     @Override
@@ -181,8 +186,8 @@ public class ArticleListActivity extends ActionBarActivity implements
             } else {
                 holder.subtitleView.setText(Html.fromHtml(
                         outputFormat.format(publishedDate)
-                        + "<br/>" + " by "
-                        + mCursor.getString(ArticleLoader.Query.AUTHOR)));
+                                + "<br/>" + " by "
+                                + mCursor.getString(ArticleLoader.Query.AUTHOR)));
             }
             holder.thumbnailView.setImageUrl(
                     mCursor.getString(ArticleLoader.Query.THUMB_URL),
@@ -190,10 +195,98 @@ public class ArticleListActivity extends ActionBarActivity implements
             holder.thumbnailView.setAspectRatio(mCursor.getFloat(ArticleLoader.Query.ASPECT_RATIO));
         }
 
+        void updateList(Cursor newCursor) {
+
+            DiffUtil.DiffResult diffResult = DiffUtil.calculateDiff(new CursorCallback<Cursor>(this.mCursor, newCursor) {
+                @Override
+                public boolean areRowContentsTheSame(Cursor oldCursor, Cursor newCursor) {
+                    boolean contentsTheSame = (oldCursor.getString(ArticleLoader.Query.TITLE).equals(newCursor.getString(ArticleLoader.Query.TITLE)));
+                    Log.i("Adapter", "contents the same: " + contentsTheSame);
+                    return contentsTheSame;
+                }
+
+                // as there is no unique id in the data, we simply regard the
+                // article title as the unique id, as article titles are all different.
+                @Override
+                public boolean areCursorRowsTheSame(Cursor oldCursor, Cursor newCursor) {
+                    boolean cursorRowsTheSame = (oldCursor.getString(ArticleLoader.Query.TITLE).equals(newCursor.getString(ArticleLoader.Query.TITLE)));
+                    Log.i("Adapter", "cursor rows the same: " + cursorRowsTheSame);
+                    return cursorRowsTheSame;
+                }
+            });
+            diffResult.dispatchUpdatesTo(this);
+            this.mCursor = newCursor;
+        }
+
         @Override
         public int getItemCount() {
+            if (mCursor == null) return 0;
             return mCursor.getCount();
         }
+    }
+
+    public abstract class CursorCallback<C extends Cursor> extends DiffUtil.Callback {
+        private final C newCursor;
+        private final C oldCursor;
+
+        public CursorCallback(C newCursor, C oldCursor) {
+            this.newCursor = newCursor;
+            this.oldCursor = oldCursor;
+        }
+
+        @Override
+        public int getOldListSize() {
+            return oldCursor == null ? 0 : oldCursor.getCount();
+        }
+
+        @Override
+        public int getNewListSize() {
+            return newCursor == null? 0 : newCursor.getCount();
+        }
+
+        @Override
+        public final boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+            return oldCursor.getColumnCount() == newCursor.getColumnCount() && moveCursorsToPosition(oldItemPosition, newItemPosition) && areCursorRowsTheSame(oldCursor, newCursor);
+        }
+
+
+        @Override
+        public final boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
+            return oldCursor.getColumnCount() == newCursor.getColumnCount() && moveCursorsToPosition(oldItemPosition, newItemPosition) && areRowContentsTheSame(oldCursor, newCursor);
+        }
+
+        @Nullable
+        @Override
+        public final Object getChangePayload(int oldItemPosition, int newItemPosition) {
+            moveCursorsToPosition(oldItemPosition, newItemPosition);
+            return getChangePayload(newCursor, oldCursor);
+        }
+        @Nullable
+        public Object getChangePayload(C newCursor, C oldCursor) {
+            return null;
+        }
+
+        private boolean moveCursorsToPosition(int oldItemPosition, int newItemPosition) {
+            boolean newMoved = newCursor.moveToPosition(newItemPosition);
+            boolean oldMoved = oldCursor.moveToPosition(oldItemPosition);
+            return newMoved && oldMoved;
+        }
+        /** Cursors are already moved to positions where you should obtain data by row.
+         *  Checks if contents at row are same
+         *
+         * @param oldCursor Old cursor object
+         * @param newCursor New cursor object
+         * @return See DiffUtil
+         */
+        public abstract boolean areRowContentsTheSame(Cursor oldCursor, Cursor newCursor);
+
+        /** Cursors are already moved to positions where you should obtain data from row
+         *  Checks if rows are the same, ideally, check by unique id
+         * @param oldCursor Old cursor object
+         * @param newCursor New cursor object
+         * @return See DiffUtil
+         */
+        public abstract boolean areCursorRowsTheSame(Cursor oldCursor, Cursor newCursor);
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
@@ -203,9 +296,9 @@ public class ArticleListActivity extends ActionBarActivity implements
 
         public ViewHolder(View view) {
             super(view);
-            thumbnailView = (DynamicHeightNetworkImageView) view.findViewById(R.id.thumbnail);
-            titleView = (TextView) view.findViewById(R.id.article_title);
-            subtitleView = (TextView) view.findViewById(R.id.article_subtitle);
+            thumbnailView = view.findViewById(R.id.thumbnail);
+            titleView = view.findViewById(R.id.article_title);
+            subtitleView = view.findViewById(R.id.article_subtitle);
         }
     }
 }
